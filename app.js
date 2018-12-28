2365a361578ca235de9a66577ce3146997353164
clsulli <clsulli@siue.edu>
2018-12-27 20:42:35 -0600

    Changed server file name to 'app' from 'index'

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('HEY!');
-});
 
+var landing = require('./routes/landing.js');
+
+app.use('/', landing);
 app.listen(3000, () => console.log('Server running on port 3000'));
+
